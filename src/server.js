ecbcea5fadcff0522ad22aff5ad12129d83650f6
Chinegua <alu0100830064@ull.edu.es>
2017-03-02 21:29:59 +0000

    Añadiendo link iaas Aitor

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const path = require('path');
 
-app.set('port',(process.env.PORT || 8080));
+app.set('port',(process.env.PORT || 8081));
 
 app.use(express.static(__dirname + '/_book'))
 
